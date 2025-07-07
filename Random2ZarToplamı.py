d310954e933d4154f75f88d72a1221ef95dc067a
exelans001 <altunkayaa8511@gmail.com>
2025-07-07 14:55:10 +0300

    Create Random2ZarToplamı.py

---
+++ Random2ZarToplamı.py
@@ -0,0 +1,11 @@
+#Rastgele atılan 2 zarın toplamı
+
+import random
+
+zar1=random.randint(1, 6)
+zar2=random.randint(1, 6)
+ztoplam= zar1 + zar2
+
+print(f"1.Zar: {zar1}")
+print(f"2.Zar: {zar2}")
+print(f"Zar Toplamı: {ztoplam}")
